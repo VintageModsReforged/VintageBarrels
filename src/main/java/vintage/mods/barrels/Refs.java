cba95a03d0a5121f067c25e3c941ee9b99ec5410
CrossVas <cross.wanted.1.4@gmail.com>
2024-12-08 17:05:07 +0900

    added deps

--- src/main/java/vintage/mods/barrels/Refs.java
+++ src/main/java/vintage/mods/barrels/Refs.java
@@ -5,7 +5,7 @@ public class Refs {
     public static final String ID = "vintagebarrels";
     public static final String NAME = "Vintage Barrels";
     public static final String VERSION = "1.4.7-1.0.1";
-    public static final String DEPS = "";
+    public static final String DEPS = "required-after:VintageCore;";
     public static final String MC_VERSION = "[1.4.7]";
 
     public static final String PROXY_CLIENT = "vintage.mods.barrels.proxy.ClientProxy";
