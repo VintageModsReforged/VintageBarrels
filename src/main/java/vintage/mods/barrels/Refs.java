ba026bf08de096b24caffc9d0830834a603ec0aa
CrossVas <cross.wanted.1.4@gmail.com>
2024-12-19 13:16:16 +0900

    version index update

--- src/main/java/vintage/mods/barrels/Refs.java
+++ src/main/java/vintage/mods/barrels/Refs.java
@@ -7,7 +7,7 @@ public class Refs {
 
     public static final String ID = "vintagebarrels";
     public static final String NAME = "Vintage Barrels";
-    public static final String VERSION = "1.5.2-1.0.4";
+    public static final String VERSION = "1.5.2-1.0.5";
     public static final String DEPS = "required-after:VintageCore;after:inventorytweaks;";
     public static final String MC_VERSION = "[1.5.2]";
 
